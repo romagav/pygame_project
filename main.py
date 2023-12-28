4822d944111360dcdae4910e4e0f3aeab1675d90
lozbe <lozben321@gmail.com>
2023-12-28 22:25:26 +0700

    1

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+import pygame
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def load_level(filename):
+    filename = "data/" + filename
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
+
+tile_width = tile_height = 50
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'i, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
 
-# Press the green button in the gutter to run the script.
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    pygame.init()
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+    pygame.quit()
