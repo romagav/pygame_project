f09d2427f6fe7ffd0c7ca207494f49670eba1a1a
romagav <rom.gavlyasek9@gmail.com>
2023-12-29 00:31:59 +0700

    final_prototype

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import pygame
 import sys
 import os
 
-size = width, height = 800, 400
-screen = pygame.display.set_mode(size)
+pygame.init()
 
 
 def load_level(filename):
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
     max_width = max(map(len, level_map))
-    return list(map(lambda x: x.ljust(max_width, ' '), level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
 def load_image(name, colorkey=None):
@@ -29,13 +28,33 @@ def load_image(name, colorkey=None):
     return image
 
 
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '!':
+                Tile('enemy', x, y)
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
+image = pygame.image.load('sand.png').convert_alpha()
+screen.blit(image, (0, 0))
+pygame.mixer.music.load('sizif.mp3')
+pygame.mixer.music.play()
 player = None
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
 
 tile_images = {
-    'wall': load_image('bush.png')
+    'wall': load_image('bush.png'),
+    'enemy': load_image('enemy.png')
 }
 player_image = load_image('ball.png')
 
@@ -46,7 +65,8 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = tile_images[tile_type]
-        self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
 
 class Player(pygame.sprite.Sprite):
@@ -54,19 +74,7 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = player_image
         self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '#':
-                Tile('wall', x, y)
-                pygame.display.flip()
-            elif level[y][x] == '@':
-                new_player = Player(x, y)
-    return new_player, x, y
+            tile_width * pos_x, tile_height * pos_y)
 
 
 player, level_x, level_y = generate_level(load_level('first_level.txt'))
@@ -75,6 +83,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYUP:
+            if event.key == 1073741906:
+                player.rect.top -= 10
+            if event.key == 1073741905:
+                player.rect.top += 10
+            if event.key == 1073741903:
+                player.rect.left += 10
+            if event.key == 1073741904:
+                player.rect.left -= 10
+        screen.blit(image, (0, 0))
+        pygame.display.flip()
     all_sprites.draw(screen)
     pygame.display.flip()
 pygame.quit()
