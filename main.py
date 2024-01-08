e5829235d3e2c65d3678ac9f66bec19d159d1408
gav_shiza <rom.gavlyasek9@gmail.com>
2024-01-08 15:49:47 +0700

    updated moving

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def load_image(name, colorkey=None):
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
-    image = pygame.image.load(fullname)
+    img = pygame.image.load(fullname)
     if colorkey is not None:
-        image = image.convert()
+        img = img.convert()
         if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
+            colorkey = img.get_at((0, 0))
+        img.set_colorkey(colorkey)
     else:
-        image = image.convert_alpha()
-    return image
+        img = img.convert_alpha()
+    return img
 
 
 def generate_level(level):
@@ -44,9 +44,12 @@ def generate_level(level):
     return new_player, x, y
 
 
-size = width, height = 800, 400
+fps = 60
+clock = pygame.time.Clock()
+size = width, height = 500, 500
 screen = pygame.display.set_mode(size)
 image = pygame.image.load('sand.png').convert_alpha()
+image = pygame.transform.scale(image, (width, height))
 screen.blit(image, (0, 0))
 pygame.mixer.music.load('sizif.mp3')
 pygame.mixer.music.play()
@@ -93,21 +96,41 @@ class WinTile(pygame.sprite.Sprite):
 
 
 player, level_x, level_y = generate_level(load_level('first_level.txt'))
+up, down, right, left = False, False, False, False
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == 1073741906:
+                up = True
+            if event.key == 1073741905:
+                down = True
+            if event.key == 1073741903:
+                right = True
+            if event.key == 1073741904:
+                left = True
+
         if event.type == pygame.KEYUP:
             if event.key == 1073741906:
-                player.rect.top -= 10
+                up = False
             if event.key == 1073741905:
-                player.rect.top += 10
+                down = False
             if event.key == 1073741903:
-                player.rect.left += 10
+                right = False
             if event.key == 1073741904:
-                player.rect.left -= 10
-        screen.blit(image, (0, 0))
+                left = False
+    if up:
+        player.rect.top -= 1
+    if down:
+        player.rect.top += 1
+    if right:
+        player.rect.left += 1
+    if left:
+        player.rect.left -= 1
+    screen.blit(image, (0, 0))
+
     if pygame.sprite.spritecollideany(player, tiles_group):
         screen.blit(image, (0, 0))  # здесь нужна картинка конец игры
         pygame.display.flip()
@@ -121,6 +144,7 @@ while running:
         player.kill()
         # следующий уровень начинается здесь
         player, level_x, level_y = generate_level(load_level('first_level.txt'))
+    clock.tick(fps)
     all_sprites.draw(screen)
     pygame.display.flip()
 pygame.quit()
